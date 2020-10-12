f0909396bc0f82a22290b2bea713b2c9bc3a0f4d
edidk7 <d.ignacio292@gmail.com>
2020-10-11 21:57:08 -0300

    Bootstrap 5 instalado (Closes #1)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="bg-secondary text-light">
+      <div class="container">
+        <div class="row py-5 justify-content-center">
+          <div class="col-md-6">
+            <h3 class="font-bold">Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur</p>
+            <button class="btn btn-danger">Go away!</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
